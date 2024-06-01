b4231fac48bf41b19fcfe000d494c40eda312ec1
benjaminpaniagua <benjaminjosepaniaguarojas@gmail.com>
2024-05-31 18:07:37 -0600

    MajorsCourseSeeder ready

--- database/seeders/MajorsCourseSeeder.php
+++ database/seeders/MajorsCourseSeeder.php
@@ -22,25 +22,17 @@ class MajorsCourseSeeder extends Seeder
         // Computer Science's Courses
         MajorsCourse::create(['majors_id' => 1, 'courses_id' => 2]); // Data Analysis (id 2)
         MajorsCourse::create(['majors_id' => 1, 'courses_id' => 3]); // Advanced Web Development (id 3)
-        MajorsCourse::create(['majors_id' => 1, 'courses_id' => 4]); // Introduction to Programming (id 4)
-        MajorsCourse::create(['majors_id' => 1, 'courses_id' => 5]); // Computer Security (id 5)
 
         // Medicine's Courses
-        MajorsCourse::create(['majors_id' => 2, 'courses_id' => 6]); // Anatomy and Physiology (id 6)
-        MajorsCourse::create(['majors_id' => 2, 'courses_id' => 7]); // Pathology (id 7)
-        MajorsCourse::create(['majors_id' => 2, 'courses_id' => 8]); // Pharmacology (id 8)
-        MajorsCourse::create(['majors_id' => 2, 'courses_id' => 9]); // Clinical Medicine (id 9)
+        MajorsCourse::create(['majors_id' => 2, 'courses_id' => 4]); // Anatomy and Physiology (id 4)
+        MajorsCourse::create(['majors_id' => 2, 'courses_id' => 5]); // Pathology (id 5)
 
         // Architecture's Courses
-        MajorsCourse::create(['majors_id' => 3, 'courses_id' => 10]); // Architectural Design (id 10)
-        MajorsCourse::create(['majors_id' => 3, 'courses_id' => 11]); // Building Materials and Construction (id 11)
-        MajorsCourse::create(['majors_id' => 3, 'courses_id' => 12]); // History of Architecture (id 12)
-        MajorsCourse::create(['majors_id' => 3, 'courses_id' => 13]); // Urban Planning (id 13)
+        MajorsCourse::create(['majors_id' => 3, 'courses_id' => 6]); // Architectural Design (id 6)
+        MajorsCourse::create(['majors_id' => 3, 'courses_id' => 7]); // History of Architecture (id 7)
 
         // Biology's Courses
-        MajorsCourse::create(['majors_id' => 4, 'courses_id' => 14]); // Molecular Biology (id 14)
-        MajorsCourse::create(['majors_id' => 4, 'courses_id' => 15]); // Ecology and Environmental Biology (id 15)
-        MajorsCourse::create(['majors_id' => 4, 'courses_id' => 16]); // Genetics (id 16)
-        MajorsCourse::create(['majors_id' => 4, 'courses_id' => 17]); // Microbiology (id 17)
+        MajorsCourse::create(['majors_id' => 4, 'courses_id' => 8]); // Molecular Biology (id 8)
+        MajorsCourse::create(['majors_id' => 4, 'courses_id' => 9]); // Ecology and Environmental Biology (id 9)
     }
 }
