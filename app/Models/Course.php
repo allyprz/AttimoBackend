56e1d460a317e1e1ffcf4e714fae1507ef66e6e4
PuzzledStone <ianxd03@gmail.com>
2024-06-01 12:48:11 -0600

    deleted local migration

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'acronyms',
+        'image',
+        'consultations',
+    ];
 }
--- database/migrations/2024_05_31_054732_create_courses_table.php
+++ database/migrations/2024_05_31_054732_create_courses_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table ->string('name');
-            $table ->string('description');
+            $table ->text('description');
             $table ->string('acronyms');
             $table ->string('image');
             $table ->string('consultations');
